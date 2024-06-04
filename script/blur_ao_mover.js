7d97b5d01968ec70ede12f9468cc95067ea0aad6
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2024-06-04 19:05:25 -0300

    Initial commit

---
+++ script/blur_ao_mover.js
@@ -0,0 +1,16 @@
+let blurTimeout;
+
+window.addEventListener('load', function() {
+    const blurBackground = document.querySelector('.blur-overlay');
+    blurBackground.classList.add('active');
+});
+
+window.addEventListener('scroll', function() {
+    const blurBackground = document.querySelector('.blur-overlay');
+    blurBackground.classList.add('active');
+
+    clearTimeout(blurTimeout);
+    blurTimeout = setTimeout(function() {
+        blurBackground.classList.remove('active');
+    },700); // Remover o blur após um curto período de tempo (66ms)
+});
