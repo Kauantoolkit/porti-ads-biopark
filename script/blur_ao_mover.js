95cd6798a8a3a5af7460d69ac470e90b89fbb660
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2024-06-21 16:11:11 -0300

    a

--- script/blur_ao_mover.js
+++ script/blur_ao_mover.js
@@ -1,9 +1,9 @@
 let blurTimeout;
 
-window.addEventListener('load', function() {
-    const blurBackground = document.querySelector('.blur-overlay');
-    blurBackground.classList.add('active');
-});
+//window.addEventListener('load', function() {
+//    const blurBackground = document.querySelector('.blur-overlay');
+//    blurBackground.classList.add('active');
+//});                                                                //não sei se é uma boa ativar o blur ao acessar a página
 
 window.addEventListener('scroll', function() {
     const blurBackground = document.querySelector('.blur-overlay');
@@ -12,5 +12,5 @@ window.addEventListener('scroll', function() {
     clearTimeout(blurTimeout);
     blurTimeout = setTimeout(function() {
         blurBackground.classList.remove('active');
-    },400); // Remover o blur após um curto período de tempo (66ms)
+    },400); 
 });
--- script/menu_toggle.js
+++ script/menu_toggle.js
@@ -8,11 +8,11 @@ document.getElementById('sair-link').addEventListener('click', function(event) {
     if (nav.classList.contains('active')) {
         nav.classList.remove('active');
     }
-    // Evitar que o link redirecione para outra página
+
     event.preventDefault();
 });
 
-// Adiciona eventos aos links do menu para fechar o menu ao clicar e ajustar a rolagem
+
 document.querySelectorAll('#header_nav p a').forEach(function(link) {
     link.addEventListener('click', function(event) {
         var targetId = this.getAttribute('href').substring(1);
@@ -21,17 +21,17 @@ document.querySelectorAll('#header_nav p a').forEach(function(link) {
         if (targetElement) {
             event.preventDefault();
             
-            // Calcular a posição do destino, ajustando pelo tamanho do cabeçalho fixo
+            // se tirar esse código o header fixo acaba indo por cima do container ao escrolar
             var headerHeight = document.querySelector('header').offsetHeight;
             var targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
 
-            // Rolagem suave para a posição calculada
+            // pra n teleportar a viewport pro lugar 
             window.scrollTo({
                 top: targetPosition,
                 behavior: 'smooth'
             });
 
-            // Fechar o menu
+            // 
             var nav = document.getElementById('header_nav');
             if (nav.classList.contains('active')) {
                 nav.classList.remove('active');
