bea17d9c1ea3f4b1a9ea3604573118276fa62c76
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2024-06-28 19:17:54 -0300

    a

--- script/blur_ao_mover.js
+++ script/blur_ao_mover.js
@@ -1,9 +1,5 @@
 let blurTimeout;
 
-//window.addEventListener('load', function() {
-//    const blurBackground = document.querySelector('.blur-overlay');
-//    blurBackground.classList.add('active');
-//});                                                                //não sei se é uma boa ativar o blur ao acessar a página
 
 window.addEventListener('scroll', function() {
     const blurBackground = document.querySelector('.blur-overlay');
